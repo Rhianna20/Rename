6730ff32dfa291fd2b6ef9dfc5aa7a4a4d075db2
Rhianna Stuart <rhiannastuart@hotmail.co.uk>
2020-05-24 15:17:05 +0100

    push it

--- java.js
+++ java.js
@@ -1,1 +1,16 @@
 alert("Welcome to my wonderful site!")
+
+
+
+function revealFact() {
+   if (rev==1) 
+   {
+ document.getElementById(rev).style.display='inline';
+return r=0;
+}
+ else 
+      {
+    document.getElementById(rev).style.display='none';
+    return r=1;
+           }
+        }
